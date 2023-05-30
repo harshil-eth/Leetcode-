c916a7560567919314b326ca2f255e73cd63805c
Harshil Gupta <75800986+harshil-eth@users.noreply.github.com>
2023-05-30 19:13:56 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -10,29 +10,47 @@ using namespace std;
 class Solution{
 private:
 
-    int f(int ind, int w, int val[], int wt[], vector<vector<int>> &dp) {
-    if(ind == 0) {
-        if(wt[0] <= w) return ((w/wt[0])*(val[0]));
-        return 0;
-    }
+//     int f(int ind, int w, int val[], int wt[], vector<vector<int>> &dp) {
+//     if(ind == 0) {
+//         if(wt[0] <= w) return ((w/wt[0])*(val[0]));
+//         return 0;
+//     }
 
-    if(dp[ind][w] != -1)
-        return dp[ind][w]; 
+//     if(dp[ind][w] != -1)
+//         return dp[ind][w]; 
 
-    int notpick = 0 + f(ind-1, w, val, wt, dp);
-    int pick = -1e9;
-    if(wt[ind] <= w) 
-        pick = val[ind] + f(ind, w-wt[ind], val, wt, dp);
+//     int notpick = 0 + f(ind-1, w, val, wt, dp);
+//     int pick = -1e9;
+//     if(wt[ind] <= w) 
+//         pick = val[ind] + f(ind, w-wt[ind], val, wt, dp);
 
-    return dp[ind][w] = max(pick, notpick);
-}
+//     return dp[ind][w] = max(pick, notpick);
+// }
 
 public:
     int knapSack(int n, int w, int val[], int wt[])
     {
         // code here
-    vector<vector<int>> dp(n, vector<int> (w+1, -1));
-    return f(n-1, w, val, wt, dp);
+        // vector<vector<int>> dp(n, vector<int> (w+1, -1));
+        // return f(n-1, w, val, wt, dp);
+    
+        vector<vector<int>> dp(n, vector<int> (w+1, 0));
+        for(int j=wt[0];j<w+1;j++) {
+            dp[0][j] = ((j/wt[0])*(val[0]));
+        }
+        
+        for(int i=1;i<n;i++) {
+            for(int j=0;j<w+1;j++) {
+                int notpick = 0 + dp[i-1][j];
+                int pick = -1e9;
+                if(wt[i] <= j) 
+                    pick = val[i] + dp[i][j-wt[i]];
+                    
+                dp[i][j] = max(pick, notpick);
+            }
+        }
+        
+        return dp[n-1][w];
     }
 };
 
