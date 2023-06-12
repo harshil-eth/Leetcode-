41ad28687ca04c40d40382d896f3040e63ad2264
Harshil Gupta <75800986+harshil-eth@users.noreply.github.com>
2023-06-12 22:37:15 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -11,21 +11,21 @@ using namespace std;
 class Solution{
   private:
   
-    int f(int ind,  int wt, int val[], vector<vector<int>> &dp) {
-        if(ind == 0) {
-            return wt*val[0];
-        }
-        if(dp[ind][wt] != -1)
-            return dp[ind][wt];
+    // int f(int ind,  int wt, int val[], vector<vector<int>> &dp) {
+    //     if(ind == 0) {
+    //         return wt*val[0];
+    //     }
+    //     if(dp[ind][wt] != -1)
+    //         return dp[ind][wt];
         
-        int notpick = 0 + f(ind-1, wt, val, dp);
-        int pick = -1e8;
-        int rod = ind+1;
-        if(rod <= wt) 
-            pick = val[ind] + f(ind, wt-rod, val, dp);
+    //     int notpick = 0 + f(ind-1, wt, val, dp);
+    //     int pick = -1e8;
+    //     int rod = ind+1;
+    //     if(rod <= wt) 
+    //         pick = val[ind] + f(ind, wt-rod, val, dp);
             
-        return dp[ind][wt] = max(pick, notpick);
-    }
+    //     return dp[ind][wt] = max(pick, notpick);
+    // }
   
   public:
     int cutRod(int val[], int n) {
