e2faa2659bfda70ded7da426e1f709c8c3aba355
Harshil Gupta <75800986+harshil-eth@users.noreply.github.com>
2023-07-21 12:59:11 +0530

    Added solution - LeetHub

--- Rotate by 90 degree - GFG/rotate-by-90-degree.cpp
+++ Rotate by 90 degree - GFG/rotate-by-90-degree.cpp
@@ -1,21 +1,17 @@
 //{ Driver Code Starts
-//Initial template for C++
-
-#include<bits/stdc++.h>
-using namespace std;
-void rotate (vector<vector<int> >& matrix);
-
+#include <bits/stdc++.h> 
+using namespace std; 
 
 // } Driver Code Ends
-//User function template for C++
 
-/* matrix : given input matrix, you are require 
- to change it in place without using extra space */
-void rotate(vector<vector<int> >& arr)
-{
-    // Your code goes here
-    int n=arr.size();
-    for(int i=0; i<n; i++) {
+class Solution
+{   
+    public:
+    //Function to rotate matrix anticlockwise by 90 degrees.
+    void rotateby90(vector<vector<int> >& arr, int n) 
+    { 
+        // code here 
+        for(int i=0; i<n; i++) {
 	        for(int j=0; j<i; j++) {
 	            swap(arr[i][j], arr[j][i]);
 	        }
@@ -26,36 +22,37 @@ void rotate(vector<vector<int> >& arr)
 	            swap(arr[i][j], arr[n-i-1][j]);
 	        }
 	    }
-}
+    } 
+};
 
 
 //{ Driver Code Starts.
-
-int main()
-{
+int main() {
     int t;
-    cin>>t; 
-    while(t--)
+    cin>>t;
+    
+    while(t--) 
     {
         int n;
-        cin>>n; 
-        vector<vector<int> > matrix(n);
+        cin>>n;
+        vector<vector<int> > matrix(n); 
+
         for(int i=0; i<n; i++)
         {
-            matrix[i].resize(n);
-            for(int j=0; j<n; j++)
-                cin>>matrix[i][j]; 
+            matrix[i].assign(n, 0);
+            for( int j=0; j<n; j++)
+            {
+                cin>>matrix[i][j];
+            }
         }
-        rotate(matrix);
+
+        Solution ob;
+        ob.rotateby90(matrix, n);
         for (int i = 0; i < n; ++i)
-        {
-            for(int j=0; j<n; j++)
+            for (int j = 0; j < n; ++j)
                 cout<<matrix[i][j]<<" ";
-            cout<<"\n";
-        }
+        cout<<endl;
     }
     return 0;
 }
-
-
 // } Driver Code Ends
