62292b8247f723c39c9b7ef259405990b716c14f
Harshil Gupta <75800986+harshil-eth@users.noreply.github.com>
2023-07-25 16:20:01 +0530

    Added solution - LeetHub

--- Stock span problem - GFG/stock-span-problem.cpp
+++ Stock span problem - GFG/stock-span-problem.cpp
@@ -6,44 +6,40 @@ using namespace std;
 // } Driver Code Ends
 
 
-
-
 class Solution
 {
     public:
     //Function to calculate the span of stockâ€™s price for all n days.
-    vector <int> calculateSpan(int price[], int n)
+    vector <int> calculateSpan(int arr[], int n)
     {
+        stack<pair<int, int>> s;
+        vector<int> ans;
+        
+        for(int i=0; i<n; i++) {
+            if(s.size() == 0) ans.push_back(-1);
+            else if(s.size() > 0 && s.top().first > arr[i]) ans.push_back(s.top().second);
+            else if(s.size() >0 && s.top().first <= arr[i]) {
+                while(s.size() > 0 && s.top().first <= arr[i]) {
+                    
+                    s.pop();
+                }
+                
+                if(s.size() == 0) ans.push_back(-1);
+                else ans.push_back(s.top().second);
+            }
+            
+            s.push({arr[i], i});
+        }
+        
+        vector<int> res(n);
+        for(int i=0; i<n; i++) {
+            res[i] = i - ans[i];
+        }
+        return res;
        // Your code here
-       stack<pair<int, int> > s;
-       vector<int> nglIndex;
        
-       for(int i=0;i<n;i++) {
-           if(s.size() == 0) nglIndex.push_back(-1);
-           
-           else if(s.size() > 0 && s.top().first > price[i]) nglIndex.push_back(s.top().second);
-           
-           else if(s.size() > 0 && s.top().first <= price[i]) {
-               while(s.size() > 0 && s.top().first <= price[i]) {
-                   s.pop();
-               }
-               
-               if(s.size() == 0) nglIndex.push_back(-1);
-               else nglIndex.push_back(s.top().second);
-           }
-           
-           s.push(make_pair(price[i], i));
-       }
-       
-       vector<int> ans;
-       for(int i=0;i<n;i++) {
-           ans.push_back(i-nglIndex[i]);
-       }
-       
-       return ans;
     }
 };
-
 
 
 
