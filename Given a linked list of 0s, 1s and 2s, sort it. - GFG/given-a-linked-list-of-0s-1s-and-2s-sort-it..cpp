e627328463feccf38b754292eb837ecc3937f31a
Harshil Gupta <75800986+harshil-eth@users.noreply.github.com>
2023-09-16 16:41:56 +0530

    Added solution - LeetHub

--- Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..cpp
+++ Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..cpp
@@ -29,43 +29,72 @@ struct Node *start = NULL;
 };
 
 */
+
 class Solution
 {
-    private:
-        void insertTotail(Node* &head, Node* &tail, int &val) {
-            if(head == NULL) {
-                Node* newnode = new Node(val);
-                head = newnode;
-                tail = newnode;
-            }
-            else {
-                Node* newnode = new Node(val);
-                tail->next = newnode;
-                tail = newnode;
-            }
-        }
-    
     public:
+    void insertattail(Node* &tail, Node* &curr)
+{
+    tail->next = curr;
+    tail = curr;
+}
+    
     //Function to sort a linked list of 0s, 1s and 2s.
     Node* segregate(Node *head) {
         
         // Add code here
-        vector<int> nums;
-        Node* t = head;
-        while(t) {
-            nums.push_back(t->data);
-            t = t->next;
+        Node* zerohead = new Node(-1);
+    Node* zerotail = zerohead;
+    Node* onehead = new Node(-1);
+    Node* onetail = onehead;
+    Node* twohead = new Node(-1);
+    Node* twotail = twohead;
+
+    Node* curr = head;
+
+    while(curr != NULL)
+    {
+        int value = curr->data;
+
+        if(value == 0)
+        {
+            insertattail(zerotail,curr);
         }
-        
-        sort(nums.begin(), nums.end());
-        
-        Node* anshead = NULL;
-        Node* anstail = NULL;
-        for(int i=0; i<nums.size(); i++) {
-            insertTotail(anshead, anstail, nums[i]);
+        else if(value == 1)
+        {
+            insertattail(onetail, curr);
         }
-        
-        return anshead;
+        else if(value == 2)
+        {
+            insertattail(twotail, curr);
+        }
+
+        curr = curr->next;
+    }
+
+    //ab merge karo
+
+    if(onehead->next != NULL)
+    {
+        zerotail->next = onehead->next;
+    }
+    else
+    {
+        zerotail->next = twohead->next;
+    }
+
+    onetail->next = twohead->next;
+    twotail->next = NULL;
+
+    //head setup
+    head = zerohead->next;
+
+    //delete dummy nodes
+    delete zerohead;
+    delete onehead;
+    delete twohead;
+
+    return head;
         
     }
 };
