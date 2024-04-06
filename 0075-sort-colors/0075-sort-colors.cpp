b3c017b0af16db7b1271a2de38f95e579088c096
Harshil Gupta <harshilg2012@gmail.com>
2024-04-06 15:00:59 +0530

    Time: 5 ms (17.82%), Space: 9.6 MB (95.50%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void sortColors(vector<int>& arr) {
+        int n = arr.size();
+        int lo =0, hi = n-1, mid=  0;
+        
+        while(mid <= hi) {
+            if(arr[mid] == 0) {
+                swap(arr[lo], arr[mid]);
+                lo++, mid++;
+            }
+            else if(arr[mid] == 1) mid++;
+            else {
+                swap(arr[hi], arr[mid]);
+                hi--;
+            }
+        }
+    }
+};
